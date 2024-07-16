8ce9e4627a875a737c908585b0fac3bd5a84ed10
chr1ce <chris.hyde.w@gmail.com>
2024-07-16 04:24:56 -0400

    Explicitly name base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
-  root,
+  base: './',
   build: {
     outDir,
     emptyOutDir: true,
